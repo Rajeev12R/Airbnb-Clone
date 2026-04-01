40ba3dd6ef413c86075b003eafc3c2806e35ce04
Rajeev Ranjan <rjranjan2112@gmail.com>
2026-04-01 14:31:44 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Rajeev Ranjan on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
